7aabdacb89293ca26be063402ebd597855a070cb
MathBriton <mathhbrito@gmail.com>
2025-02-22 16:04:16 -0300

    release 1.0

--- src/Infrastructure/obj/Debug/net9.0/Infrastructure.AssemblyInfo.cs
+++ src/Infrastructure/obj/Debug/net9.0/Infrastructure.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+858ee24dc740051be6ba9cb1a12a917148fb9daf")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+99d4c8e149da423b5153be2273ee3a646cb966ab")]
 [assembly: System.Reflection.AssemblyProductAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
