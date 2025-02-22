6d646ec6df778ff90250f976e955fd26a370d4e7
MathBriton <mathhbrito@gmail.com>
2025-02-22 15:59:36 -0300

    Subindo Services

--- src/Application/obj/Debug/net9.0/Application.AssemblyInfo.cs
+++ src/Application/obj/Debug/net9.0/Application.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+975093c75191bc285102ecedc32e2d79cef18fab")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0f50a38c92142240ac1aacf0f5465dd1e19c6944")]
 [assembly: System.Reflection.AssemblyProductAttribute("Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
